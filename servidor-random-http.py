4e6f51124672f1f75bc8d9601d8ff8dc355357fd
Javier Fernandez Morata <jmorata@gamma27.aulas.gsyc.urjc.es>
2017-02-08 12:22:28 +0100

    2º version

--- servidor-random-http.py
+++ servidor-random-http.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python>
+#!/usr/bin/python
 import socket
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mySocket.bind((socket.gethostname(), 1234))
+mySocket.bind((socket.gethostname(), 1236))
 
 mySocket.listen(5)
 
@@ -12,4 +12,6 @@ while True:
     print("HTTP request received")
     print(clientSocket.recv(1024))
     clientSocket.send("HTTP/1.1 200 OK \r\n\r\n" +
-                        "<html><body"
+                        "<html><body><h2><B>DE ECHENIQUE </B></h2>" +
+                        "<img> src='http://img01.heraldo.es/uploads/imagenes/bajacalidad/2014/10/27/_echenique_7c91153f.jpg'");
+    clientSocket.close();
