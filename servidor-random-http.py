ec6f78653b88c355d6218565f58757429e06ca84
javierfm27 <javitofm27@github.com>
2017-02-09 10:24:23 +0100

    Inacabada

--- servidor-random-http.py
+++ servidor-random-http.py
@@ -11,7 +11,5 @@ while True:
     (clientSocket, address) = mySocket.accept()
     print("HTTP request received")
     print(clientSocket.recv(1024))
-    clientSocket.send("HTTP/1.1 200 OK \r\n\r\n" +
-                        "<html><body><h2><B>DE ECHENIQUE </B></h2>" +
-                        "<img> src='http://img01.heraldo.es/uploads/imagenes/bajacalidad/2014/10/27/_echenique_7c91153f.jpg'");
+    clientSocket.send()
     clientSocket.close();
