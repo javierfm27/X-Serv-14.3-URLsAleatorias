ebc0c488641b6c26dc81e8cb7250d0db62556910
javierfm27 <javitofm27@github.com>
2017-02-13 17:33:49 +0100

    Version Final del ejercicio de URLS Aleatorias

--- servidor-random-http.py
+++ servidor-random-http.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 import socket
+import random
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mySocket.bind((socket.gethostname(), 1236))
+mySocket.bind((socket.gethostname(), 1231))
 
 mySocket.listen(5)
 
@@ -11,5 +12,9 @@ while True:
     (clientSocket, address) = mySocket.accept()
     print("HTTP request received")
     print(clientSocket.recv(1024))
-    clientSocket.send()
+    clientSocket.send('HTTP/1.1 200 OK\r\n\r\n' +
+                      '<html> <body> <section> Hola. <a href='+
+                      str(random.randint(0,1000)) +
+                      '> Dame otra </a>' +
+                      '</section> </body> </html>')
     clientSocket.close();
